8426a028dc44f2e4b90ed908495431fe85a5cec1
Albert Zhloba <albert.zhloba@gmail.com>
2020-04-06 22:16:22 +0300

    refactor: new method

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,69 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+   unsigned long n = [numbers count];
+   int i=0;
+    NSString *x = @"";
+    if (n==0) {return nil;}
+    else if (n==1) {
+        NSString *first = [NSString stringWithFormat:@"%d", [numbers.firstObject intValue]];
+        return first;
+        }
+    else if (n==2) {
+        NSString *first = [NSString stringWithFormat:@"%dx", [numbers.firstObject intValue]];
+        NSString *last;
+        if ([numbers.lastObject intValue]>0){
+            last = [NSString stringWithFormat:@" + %d", [numbers.lastObject intValue]];}
+        else {last = [NSString stringWithFormat:@" - %d", [numbers.lastObject intValue]];}
+        NSString *result;
+        if ([numbers.lastObject intValue]==0) {result = [NSString stringWithFormat:@"%@", first];}
+        else {result = [NSString stringWithFormat:@"%@%@", first, last];}
+        return result;
+        }
+    else {
+        
+        unsigned long c = n-1;
+        NSString *first = [NSString stringWithFormat:@"%dx^%lu", [numbers.firstObject intValue], n-1];
+        NSString *last;
+        if ([numbers.lastObject intValue]>0){
+            last = [NSString stringWithFormat:@" + %d", [numbers.lastObject intValue]];}
+        else {last = [NSString stringWithFormat:@" - %d", [numbers.lastObject intValue]];}
+       
+        NSString *sublast;
+        if ((([[numbers objectAtIndex:n-2] intValue]>0) && ([[numbers objectAtIndex:n-2] intValue]<1)) || ([[numbers objectAtIndex:n-2] intValue]>1)){
+            sublast = [NSString stringWithFormat:@" + %dx", [[numbers objectAtIndex:n-2] intValue]];}
+        else if ([[numbers objectAtIndex:n-2] intValue]==1){sublast = @" + x";}
+        else if ([[numbers objectAtIndex:n-2] intValue]==-1) {sublast = @" - x"; }
+    
+        else {sublast = [NSString stringWithFormat:@" - %dx", abs ([[numbers objectAtIndex:n-2] intValue])];}
+        
+        for (i=1; i<n-2; i++){
+            int long a =  [[numbers objectAtIndex:i] integerValue];
+            if (a==0) {
+                c=c-1;}
+            else if (a > 0) {
+                    c=c-1;
+                NSString *y = [NSString stringWithFormat:@" + %lux^%lu", a, c];
+                    x = [x stringByAppendingString:y];}
+            else {
+                c=c-1;
+                a = labs (a);
+                NSString *y = [NSString stringWithFormat:@" - %lux^%lu", a, c];
+                x = [x stringByAppendingString:y];
+                }
+            
+            }
+        NSString *result;
+        if ([numbers.lastObject intValue]==0) {
+            if ([[numbers objectAtIndex:n-2] intValue]==0) {
+            result = [NSString stringWithFormat:@"%@%@", first, x];}
+            else {result = [NSString stringWithFormat:@"%@%@%@", first, x, sublast];}}
+        else {
+            if ([[numbers objectAtIndex:n-2] intValue]==0) {
+            result = [NSString stringWithFormat:@"%@%@%@", first, x, last];}
+            else {result = [NSString stringWithFormat:@"%@%@%@%@", first, x, sublast, last];}}
+        return result;
+    }
+   
 }
 @end
--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -1,7 +1,47 @@
 #import "Combinator.h"
 
 @implementation Combinator
+//-(int)factorialRecursive:(int)operand
+//{
+//    if( operand == 1 || operand == 0) {
+//        return(1);
+ //   } else if( operand < 0 ) {
+ //       return(-1);
+ //   }
+    
+ //   return( operand * [self factorialRecursive:operand-1] );
+//}
+
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    int long long b =  [[array objectAtIndex:0] intValue];
+    int long long a =  [[array objectAtIndex:1] intValue];
+    int i=1;
+    int long long l;
+    int long long x=1;
+    int long long factorialn = 1; // переменная для результата
+    int long long factorialx = 1;
+    int long long factorialz = 1;
+    for (i = 1; i <= a; i++) {
+        factorialn = factorialn * i; }
+    int long long m = factorialn;
+    
+    for (x = 1; x<a; x++) {
+        factorialx = factorialx * x;
+        int long long z = a - x;
+        
+        for (l = z; l > 0; l--) {
+            factorialz = factorialz * l; }
+        int long long r = factorialx*factorialz;
+        int long long w = m/r;
+        factorialz = 1;
+        if (b>w) continue;
+        else {
+            NSNumber *res = [NSNumber numberWithInteger: x];
+            return res;
+            break;
+        }
+        
+    }
+    return nil;
 }
 @end
